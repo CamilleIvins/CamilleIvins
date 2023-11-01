122c08f6cb6fe8016d51743007bb5460fbad1c8d
Camille Ivins <camillemivins@gmail.com>
2023-11-01 16:51:51 -0600

    background options

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <header>
-    <Navbar />
-  </header>
-  <main>
-    <router-view />
-  </main>
-   <footer class="bg-dark text-light">
-    Made with 💖 by CodeWorks
-  </footer>
+  <body class="bg-background">
+    <header>
+      <Navbar />
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <footer class="text-light">
+      Made with 💖 by CodeWorks
+    </footer>
+  </body>
 </template>
 
 <script>
@@ -25,9 +27,23 @@ export default {
 }
 </script>
 <style lang="scss">
+.bg-img {
+  // Sky - purple
+  // background-image: url(https://images.unsplash.com/photo-1602498456745-e9503b30470b?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  // winding forest road
+  // background-image: url(https://images.unsplash.com/photo-1606830079880-e90bd08e9a98?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  // beach - aerial
+  // background-image: url(https://images.unsplash.com/photo-1524946274118-e7680e33ccc5?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  // Shanghai
+  background-image: url(https://images.unsplash.com/photo-1614221330834-9399e5631af3?auto=format&fit=crop&q=80&w=2067&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  object-fit: cover;
+  object-position: center;
+}
+
+
 @import "./assets/scss/main.scss";
 
-:root{
+:root {
   --main-height: calc(100vh - 32px - 64px);
 }
 
